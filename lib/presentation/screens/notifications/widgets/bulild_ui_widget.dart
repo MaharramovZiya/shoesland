556198b88f3ef34cd11fc95a22205ba3d05f14d2
Maharramli_Ziya <ziyameherremov21@gmail.com>
2024-07-27 20:39:19 +0400

    Notification screen UI is building.... V3

--- lib/presentation/screens/notifications/widgets/bulild_ui_widget.dart
+++ lib/presentation/screens/notifications/widgets/bulild_ui_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shoesland/core/constants/custom_size.dart';
 import 'package:shoesland/core/utils/app_padding.dart';
 import 'package:shoesland/presentation/screens/components/navigate_top_menu_tree.dart';
+import 'package:shoesland/presentation/widgets/general_txt_widget.dart';
 
 Widget buildUINotifications(BuildContext context) {
   return SafeArea(
@@ -8,19 +10,36 @@ Widget buildUINotifications(BuildContext context) {
     padding: AppPadding.pageWithPadding,
     child: SingleChildScrollView(
       child: Column(
-
-
-
         children: [
           //Navigate top menu
-          buildNotifactionNavigateTopMenu()
+          buildNotifactionNavigateTopMenu(),
 
-          //
-          
-          
-          
-          ],
+          //_buildUICarts
+          _buildUICarts(context)
+        ],
       ),
     ),
   ));
+}
+
+Widget _buildUICarts(context) {
+  CustomSize customSize = CustomSize(context);
+  return Container(
+    padding: const EdgeInsets.only(top: 40),
+    width: customSize.width,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          child: Column(
+            children: [
+              GeneralTextWidget("Today",
+                  textStyle: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 20))
+            ],
+          ),
+        )
+      ],
+    ),
+  );
 }
